59e586fb8c203978c191438af05da36b5b0781f1
Dustin Sallings <dustin@spy.net>
2008-09-14 16:00:53 -0700

    Added beanstalk-cleanup.

--- README.markdown
+++ README.markdown
@@ -20,3 +20,13 @@ beanstalk-queue-stats.rb watches a single beanstalk instance and shows you
 which tubes contain elements, and how fast they're changing.
 
     usage: beanstalk-queue-stats.rb host:11300
+
+### beanstalk-cleanup.rb
+
+beanstalk-cleanup.rb is a continuously edited one-off script to help clean
+stuff out of queues.  It pulls stuff out of the queue and defers, buries, or
+deletes items based on regexes over the content.
+
+    usage: vi beanstalk-cleanup.rb
+           # edit the awesome hard-coded bury and delete rules
+           beanstalk-cleanup.rb host:11300
---
+++ bin/beanstalk-cleanup.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'beanstalk-client'
+
+BS=Beanstalk::Pool.new $*
+
+saw={}
+
+loop do
+  job = BS.reserve
+  code = job.ybody[:code]
+  if saw[code] && saw[code] != job.id
+    puts "Already saw something that looked like #{job.id} (#{code}) (deleting)"
+    job.delete
+  else
+    if code =~ /like.a.chisled.greek.god/
+      puts "Burying #{code}"
+      job.bury
+    elsif code =~ /who.each.posess.the.strength.of.ten.men/
+      puts "Deleting #{code}"
+      job.delete
+    else
+      puts "keeping #{job.id} (#{code})"
+      job.release(job.pri, 60)
+    end
+    saw[code] = job.id
+  end
+end
