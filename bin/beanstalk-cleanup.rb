c5b7e38f729347b98aa2be78bd029766acc3a7dc
Dustin Sallings <dustin@spy.net>
2008-11-23 12:59:04 -0800

    Abbreviate large jobs on print.

--- bin/beanstalk-cleanup.rb
+++ bin/beanstalk-cleanup.rb
@@ -7,6 +7,14 @@ BS=Beanstalk::Pool.new $*
 
 saw={}
 
+def abbrev(code, to=70)
+  if code.length > to
+    code.slice(0, to-3) + "..."
+  else
+    code
+  end
+end
+
 loop do
   job = BS.reserve
   code = case job.ybody
@@ -15,18 +23,19 @@ loop do
   else
     job.ybody.inspect
   end
+  s = abbrev(code)
   if saw[code] && saw[code] != job.id
-    puts "Already saw something that looked like #{job.id} (#{code}) (deleting)"
+    puts "Already saw something that looked like #{job.id} (#{s}) (deleting)"
     job.delete
   else
     if code =~ /like.a.chisled.greek.god/
-      puts "Burying #{code}"
+      puts "Burying #{s}"
       job.bury
     elsif code =~ /who.each.posess.the.strength.of.ten.men/
-      puts "Deleting #{code}"
+      puts "Deleting #{s}"
       job.delete
     else
-      puts "keeping #{job.id} (#{code})"
+      puts "keeping #{job.id} (#{s})"
       job.release(job.pri, 60)
     end
     saw[code] = job.id
